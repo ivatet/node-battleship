b8d13feadf2c96c13119315bd08c7ff871cb6876
xpdu <teterevkov.ivan@gmail.com>
2017-03-04 21:39:23 +0000

    upd: use mustache templating

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const http = require('http')  
+const fs = require('fs')
+const http = require('http')
+const mustache = require('mustache')
 const port = 3000
 
-const requestHandler = (request, response) => {  
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+var template = fs.readFileSync(__dirname + '/index.mst', 'utf8').toString()
+
+const requestHandler = (request, response) => {
+	templateData = {
+		url: request.url
+	}
+	response.end(mustache.to_html(template, templateData))
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+	if (err) {
+		return console.log('something bad happened', err)
+	}
 
-  console.log(`server is listening on ${port}`)
+	console.log(`server is listening on ${port}`)
 })
