6ad9e506da41f5af0318e4d4e3818f4769c72731
xpdu <teterevkov.ivan@gmail.com>
2017-03-12 19:11:01 +0000

    upd: handle client/disconnect event

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ const io = require('socket.io')(server)
 app.use(express.static('public'))
 
 io.on('connection', function(client) {
+	console.log("connect")
 	client.emit('ping', { 'msg': 'Welcome to ' + os.hostname() + '!' })
+
+	client.on('disconnect', function(client) {
+		console.log("disconnect")
+	})
 })
 
 server.listen(3000)
